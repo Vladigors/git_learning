af818d3ba97bd6a6e9832ea7d6fbaea7cd8608a0
Vladigors <83923210+Vladigors@users.noreply.github.com>
2021-06-08 12:53:22 +0300

    new commit in github at 12.52 08062021
    new commit in github at 12.52 08062021

--- func/nums.py
+++ func/nums.py
@@ -14,4 +14,5 @@ if __name__ == '__main__':
     print(plus(2, 2))
     print(minus(4, 2))
     print(mul(3, 3))
-new commit in github at 22.09 07062021
+# new commit in github at 22.09 07062021
+# new commit in github at 12.52 08062021
